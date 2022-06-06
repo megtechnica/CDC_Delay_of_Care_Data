5014207612ee928f7a6ea10e21bdf721d68581f9
megtechnica <101686829+megtechnica@users.noreply.github.com>
2022-06-06 08:09:31 -0600

    Update isolation.py

--- isolation.py
+++ isolation.py
@@ -2,9 +2,4 @@
 def isolate_total_stub(data):
     isolated_data = data[data.STUB_NAME == 'Total']
     isolated_data = isolated_data[['YEAR', 'ESTIMATE','STUB_NAME','AGE','PANEL']]
-    return isolated_data
-
-def isolate_age_stub(data):
-    isolated_data = data[data.STUB_NAME == 'Total']
-    return isolated_data
-
+    return isolaed_total_stub
